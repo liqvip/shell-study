69159f33219876b29f0a83628156b9b994d55f2b
liqiang <liqiang@makeblock.com>
2024-10-01 14:19:43 +0800

    Bash Debug 插件配置

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Shell 脚本语言学习
+
+## VSCode 插件安装
+1. Bash Debug
+2. Bash IDE
+3. ShellCheck
+4. shell-format
+5. shellman
---
+++ my.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+a="hello"
+
+if [ "$a" = "hello" ]; then
+    echo "true"
+else
+    echo "false"
+fi
+
+
+
+echo "$1"
+set
